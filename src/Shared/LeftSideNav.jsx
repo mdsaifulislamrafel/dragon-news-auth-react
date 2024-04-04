ef84d0642c1d6d7590cb0e1e0575c06dbcb92c0f
Md Saiful Islam Rafel <mdsaifulisalmrafel@gmail.com>
2024-04-04 13:02:05 +0600

    laft side navbar design complete

--- src/Shared/LeftSideNav.jsx
+++ src/Shared/LeftSideNav.jsx
@@ -1,7 +1,24 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 const LeftSideNav = () => {
+    const [categories, setCategories] = useState([]);
+
+    useEffect(() => {
+        fetch('categories.json')
+            .then(res => res.json())
+            .then(data => setCategories(data))
+    }, [])
+
     return (
-        <div>
-            <h1>This is right side nav</h1>
+        <div className="space-y-6">
+            <h2 className="text-2xl">All Categories</h2>
+            {
+                categories.map(category => <Link
+                    className="block ml-4 text-xl font-semibold"
+                    key={category.id}
+                    to={`/category/${category.id}`}
+                >{category.name}</Link>)
+            }
         </div>
     );
 };
